81f795f1acbc2bcf23576a14d90dc59f8c3b2773
Яна <yanaakipova@gmail.com>
2026-02-24 08:53:24 +0300

    new function

--- DynamicArray.c
+++ DynamicArray.c
@@ -5,7 +5,7 @@
 #define INITIAL_CAPACITY 4
 #define ZERO_SIZE 0
 
-DinamicArray* createArray(TypeInfo* type, ArrayErrors* error){
+DinamicArray* create_array(TypeInfo* type, ArrayErrors* error){
     if (type == NULL){
         if (error) *error = NULL_POINTER;
         return NULL;
@@ -28,12 +28,12 @@ DinamicArray* createArray(TypeInfo* type, ArrayErrors* error){
     arr->capacity = INITIAL_CAPACITY;
 
     if (error)*error = ARRAY_OK;
+    return arr;
 }
 
-void destroyArray(DinamicArray* arr, ArrayErrors* error){
+void destroy_array(DinamicArray* arr, ArrayErrors* error){
     if (arr == NULL){
         if (error) *error = NULL_POINTER;
-        return NULL;
     }
     
     for (unsigned int i; i < arr->size; i++){
@@ -87,6 +87,63 @@ void append(DinamicArray* arr, const void* elem, ArrayErrors* error){
     if (error) *error = ARRAY_OK;
 }
 
+void* get(const DinamicArray* arr, int index, ArrayErrors* error){
+    if (arr == NULL){
+        if (error) *error = NULL_POINTER;
+        return NULL;
+    }
+    
+    if (index < 0 || index >= arr->size){
+        if (error) *error = INDEX_OUT;
+        return NULL;
+    }
+    
+    if (error) *error = ARRAY_OK;
+    return arr->data[index];
+}
+
+int get_size(const DinamicArray* arr, ArrayErrors* error){
+    if (arr == NULL){
+        if (error) *error = NULL_POINTER;
+        return -1;
+    }
+    
+    if (error) *error = ARRAY_OK;
+    return arr->size;
+}
+
+
+void print_array(const DinamicArray* arr, ArrayErrors* error){
+    if (arr == NULL) {
+        if (error) *error = NULL_POINTER;
+        return;
+    }
+    
+    printf("[");
+    for (int i = 0; i < arr->size; i++){
+        if (arr->data[i] != NULL){
+            arr->type->print(arr->data[i], error);
+        }
+    }
+    printf("]");
+    
+    if (error) *error = ARRAY_OK;
+}
+
+void add_to_array(DinamicArray* arr, void* elem, ArrayErrors* error){
+    if (arr == NULL || elem == NULL){
+        if (error) *error = NULL_POINTER;
+        return;
+    }
+    
+    increasing_size(arr, error);
+    if (*error != ARRAY_OK) return;
+    
+    arr->data[arr->size] = elem;
+    arr->size++;
+    
+    if (error) *error = ARRAY_OK;
+}
 
 
 
--- DynamicArray.h
+++ DynamicArray.h
@@ -11,7 +11,11 @@ typedef struct DinamicArray{
     unsigned  int capacity;
 }DinamicArray;
 
-DinamicArray* createArray(TypeInfo* type, ArrayErrors* error);
-void destroyArray(DinamicArray* arr, ArrayErrors* error);
+DinamicArray* create_array(TypeInfo* type, ArrayErrors* error);
+void destroy_array(DinamicArray* arr, ArrayErrors* error);
 void increasing_size(DinamicArray* arr, ArrayErrors* error);
 void append(DinamicArray* arr, const void* elem, ArrayErrors* error);
+void* get(const DinamicArray* arr, int index, ArrayErrors* error);
+int get_size(const DinamicArray* arr, ArrayErrors* error);
+void print_array(const DinamicArray* arr, ArrayErrors* error);
+void add_to_array(DinamicArray* arr, void* elem, ArrayErrors* error);
